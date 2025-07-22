4bda16d7267be4d0eef1bf4db6244c0020f41802
sudhin <86117535+sudi050@users.noreply.github.com>
2025-07-23 01:15:21 +0530

    update on monitoring data

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, session
-from utils import get_user, get_qr_data, log_access, create_feedback
+from utils import get_user, get_qr_data, log_access, create_feedback, get_feedbacks, get_access_logs
 from datetime import datetime
 
 app = Flask(__name__)
@@ -29,11 +29,11 @@ def page(code):
     qr = get_qr_data(code)
     if not qr:
         return "Invalid QR code."
-    if code == 'final':
-        return render_template("feedback.html")
 
     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     log_access(session["user"], code, current_time)
+    if code == 'final':
+        return render_template("feedback.html")
     return render_template("page.html", qr=qr)
 
 
@@ -46,9 +46,30 @@ def submit_feedback():
     rating_event = request.form.get("rating_event")
     rating_portal = request.form.get("rating_portal")
     comments = request.form.get("comments")
-
     create_feedback(username,rating_event,rating_portal,comments)
     return render_template("thanks.html")
+
+@app.route("/view-feedback")
+def view_feedback():
+    if "user" not in session:
+        return redirect("/")
+
+    if session["user"] != "admin":
+        return "Access denied."
+
+    feedbacks = get_feedbacks()
+    return render_template("view_feedback.html", feedbacks=feedbacks)
+
+@app.route("/view-logs")
+def view_logs():
+    if "user" not in session:
+        return redirect("/")
+
+    if session["user"] != "admin":
+        return "Access denied."
+
+    logs = get_access_logs()
+    return render_template("view_logs.html", logs=logs)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=False)
--- utils.py
+++ utils.py
@@ -32,3 +32,19 @@ def create_feedback(username,rating_event,rating_portal,comments):
     """, (username, rating_event, rating_portal, comments))
     con.commit()
     con.close()
+
+def get_feedbacks():
+    con = sqlite3.connect("data.db")
+    cur = con.cursor()
+    cur.execute("SELECT username, rating_event, rating_portal, comments FROM feedback")
+    feedbacks = cur.fetchall()
+    con.close()
+    return feedbacks
+
+def get_access_logs():
+    con = sqlite3.connect("data.db")
+    cur = con.cursor()
+    cur.execute("SELECT username, Loc, timestamp FROM access_logs ORDER BY timestamp DESC")
+    logs = cur.fetchall()
+    con.close()
+    return logs
